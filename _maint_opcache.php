86662972d25b18a9089ac264b55f1b1959b36264
Luis Mosko <luismosko@gmail.com>
2026-07-01 13:46:26 +0000

    chore: remove helper temporario de reset OPcache (pos v1.3.0)

--- _maint_opcache.php
+++
@@ -1,4 +0,0 @@
-<?php
-if (($_GET['k'] ?? '') !== 'km_reset_7c4e1a92') { http_response_code(404); exit; }
-header('Content-Type: application/json');
-echo json_encode(['reset' => function_exists('opcache_reset') ? opcache_reset() : 'no_func']);
